93683e773a49682b92b5c136aff36eb7e139ea8d
Uwe Barnstein <uwe@barnstein.de>
2026-03-03 08:04:42 +0100

    Update LinBusListener_esp32_arduino.cpp

--- components/truma_inetbox/LinBusListener_esp32_arduino.cpp
+++ components/truma_inetbox/LinBusListener_esp32_arduino.cpp
@@ -4,8 +4,13 @@
 #include "driver/uart.h"
 #include "soc/uart_struct.h"
 #include "soc/uart_reg.h"
-#include "esphome/components/uart/uart_component_esp_idf.h"
-#define ESPHOME_UART uart::IDFUARTComponent
+#ifdef CUSTOM_ESPHOME_UART
+#include "esphome/components/uart/truma_uart_component_esp32_arduino.h"
+#define ESPHOME_UART uart::truma_ESP32ArduinoUARTComponent
+#else
+#define ESPHOME_UART uart::ESP32ArduinoUARTComponent
+#endif // CUSTOM_ESPHOME_UART
+#include "esphome/components/uart/uart_component_esp32_arduino.h"
 
 namespace esphome {
 namespace truma_inetbox {
@@ -16,67 +21,53 @@ static const char *const TAG = "truma_inetbox.LinBusListener";
 
 void LinBusListener::setup_framework() {
   auto uartComp = static_cast<ESPHOME_UART *>(this->parent_);
-  uart_port_t uart_num = static_cast<uart_port_t>(uartComp->get_hw_serial_number());
+
+  auto uart_num = uartComp->get_hw_serial_number();
+  auto hw_serial = uartComp->get_hw_serial();
+
+  // Extract from `uartSetFastReading` - Can't call it because I don't have access to `uart_t` object.
 
   // Tweak the fifo settings so data is available as soon as the first byte is recieved.
   // If not it will wait either until fifo is filled or a certain time has passed.
   uart_intr_config_t uart_intr;
   uart_intr.intr_enable_mask =
       UART_RXFIFO_FULL_INT_ENA_M | UART_RXFIFO_TOUT_INT_ENA_M;  // only these IRQs - no BREAK, PARITY or OVERFLOW
+  // UART_RXFIFO_FULL_INT_ENA_M | UART_RXFIFO_TOUT_INT_ENA_M | UART_FRM_ERR_INT_ENA_M |
+  // UART_RXFIFO_OVF_INT_ENA_M | UART_BRK_DET_INT_ENA_M | UART_PARITY_ERR_INT_ENA_M;
   uart_intr.rxfifo_full_thresh =
       1;  // UART_FULL_THRESH_DEFAULT,  //120 default!! aghh! need receive 120 chars before we see them
   uart_intr.rx_timeout_thresh =
       10;  // UART_TOUT_THRESH_DEFAULT,  //10 works well for my short messages I need send/receive
   uart_intr.txfifo_empty_intr_thresh = 10;  // UART_EMPTY_THRESH_DEFAULT
-  uart_intr_config(uart_num, &uart_intr);
+  uart_intr_config((uart_port_t)uart_num, &uart_intr);
 
-  // Creating UART event Task
-  xTaskCreatePinnedToCore(LinBusListener::uartEventTask_,
-                          "uart_event_task",                      // name
-                          ARDUINO_SERIAL_EVENT_TASK_STACK_SIZE,   // stack size (in words)
-                          this,                                   // input params
-                          24,                                     // priority
-                          &this->uartEventTaskHandle_,            // handle
-                          ARDUINO_SERIAL_EVENT_TASK_RUNNING_CORE  // core
-  );
-  if (this->uartEventTaskHandle_ == NULL) {
-    ESP_LOGE(TAG, " -- UART%d Event Task not created!", uart_num);
-  }
+  hw_serial->onReceive([this]() { this->onReceive_(); }, false);
+  hw_serial->onReceiveError([this](hardwareSerial_error_t val) {
+    // Ignore any data present in buffer
+    this->clear_uart_buffer_();
+    if (val == UART_BREAK_ERROR) {
+      // If the break is valid the `onReceive` is called first and the break is handeld. Therfore the expectation is
+      // that the state should be in waiting for `SYNC`.
+      if (this->current_state_ != READ_STATE_SYNC) {
+        this->current_state_ = READ_STATE_BREAK;
+      }
+      return;
+    }
+  });
 
   // Creating LIN msg event Task
   xTaskCreatePinnedToCore(LinBusListener::eventTask_,
                           "lin_event_task",         // name
-                          ARDUINO_SERIAL_EVENT_TASK_STACK_SIZE,                     // stack size (in words)
+                          4096,                     // stack size (in words)
                           this,                     // input params
                           2,                        // priority
                           &this->eventTaskHandle_,  // handle
-                          ARDUINO_SERIAL_EVENT_TASK_RUNNING_CORE                         // core
+                          0                         // core
   );
 
   if (this->eventTaskHandle_ == NULL) {
     ESP_LOGE(TAG, " -- LIN message Task not created!");
   }
-}
-
-void LinBusListener::uartEventTask_(void *args) {
-  LinBusListener *instance = (LinBusListener *) args;
-  
-  // 1. WICHTIG: Wir warten 2 Sekunden (2000 ms), bevor der Task überhaupt anfängt!
-  // So hat ESPHome genug Zeit, den UART-Port und das WLAN in Ruhe hochzufahren.
-  vTaskDelay(pdMS_TO_TICKS(2000));
-  
-  for (;;) {
-    // 2. Sicherheits-Check: Nur abfragen, wenn die Instanz wirklich existiert
-    if (instance != nullptr && instance->available() > 0) {
-      instance->onReceive_();
-    }
-    
-    // 3. Atempause für den Watchdog (10 Millisekunden statt 2). 
-    // Das ist für den LIN-Bus schnell genug, aber entlastet die CPU massiv.
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-  
-  vTaskDelete(NULL);
 }
 
 void LinBusListener::eventTask_(void *args) {
